59c5457aab7e461d05d048f7dc2e13bf7052bbc6
Prinsi Malaviya <malviyaprinsi@gmail.com>
2025-07-18 12:52:08 +0530

    Initial commit on newbranch

--- HRM desktop application/Program.cs
+++ HRM desktop application/Program.cs
@@ -18,7 +18,7 @@ namespace HRM_desktop_application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new New_MainForm());
+            Application.Run(new Loginpage());
         }
     }
 }
